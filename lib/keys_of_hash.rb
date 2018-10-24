f87a6d8cd318d93695fb82b57af905e9e473e60a
Emily Asch <e.asch@me.com>
2018-10-24 20:38:17 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(arguments)
     # code goes here
    self.map do |k, v|
-     key if arguments.include?(value)
+     key if arguments.include?(v)
    end.compact
    end
   
